788fdfa255345ecd0fbaa04b6f1e4957d26350d2
aryan123mittal <136236033+aryan123mittal@users.noreply.github.com>
2024-09-07 14:34:38 +0530

    Add files via upload

---
+++ MaxMin.java
@@ -0,0 +1,20 @@
+public class MaxMin
+{
+   public static void main(String args[])
+   {
+       int arr[] = {34,15,19,25,12,78};
+       int max = arr[0]; 
+       int min = arr[0];
+       for(int i=0; i<arr.length; i++) { 
+           if(min > arr[i])
+              min = arr[i];
+           if(max < arr[i])
+              max = arr[i];
+       }
+       System.out.println("minimum element in array is " + min);
+       System.out.println("maximum element in array is " + max);
+   }
+}
+
+//Time Complexity is O(n)
+//Space Complexity is O(1)
---
+++ ReverseArray.java
@@ -0,0 +1,13 @@
+public class ReverseArray
+{
+   public static void main(String args[])
+   {
+     int arr[] = {1,2,3,4,5};
+     int n=arr.length;
+     for(int i=n-1; i>=0; i--)
+       System.out.print(arr[i]+" "); 
+    }
+}
+
+//Time Complexity is O(n)
+//Space Complexity is O(1)
